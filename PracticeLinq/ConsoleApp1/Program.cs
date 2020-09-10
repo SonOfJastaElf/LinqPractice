0ee5e6d8a27e4ca09293a5d7b353afe8c43721b8
Brian Henderson <henderson.brian82@gmail.com>
2020-09-10 12:53:47 -0400

    Created a Linq practice

---
+++ PracticeLinq/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> GameList = new List<string> { "Sonic The Hedgehog", "Donkey Kong", "Final Fantasy", "R-Type", "Hyperdimension Neptunia" };
+            var length = GameList.OrderBy(name => name.Length);
+
+            foreach (var game in length)
+            {
+                Console.WriteLine(game);
+            }
+
+        }
+    }
+}
